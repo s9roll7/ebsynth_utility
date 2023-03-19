1e91627c22a944710aacaa8f4a7cdd48315de144
s9roll7 <s9roll74@gmail.com>
2023-03-19 14:20:43 +0900

    Fixed problem with ckpt_base.pth downloading every time

--- install.py
+++ install.py
@@ -1,7 +1,17 @@
 import launch
 
+def update_transparent_background():
+    from importlib.metadata import version as meta_version
+    from packaging import version
+    v = meta_version("transparent-background")
+    print("current transparent-background " + v)
+    if version.parse(v) < version.parse('1.2.3'):
+        launch.run_pip("install -U transparent-background", "update transparent-background version for Ebsynth Utility")
+
 if not launch.is_installed("transparent_background"):
     launch.run_pip("install transparent-background", "requirements for Ebsynth Utility")
+
+update_transparent_background()
 
 if not launch.is_installed("IPython"):
     launch.run_pip("install ipython", "requirements for Ebsynth Utility")
